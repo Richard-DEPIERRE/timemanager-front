884e928a25b0eca28743dd66782a2d0e4a177c89
Richard Depierre <richard.depierre@teads.tv>
2023-11-13 11:04:30 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo npm i
+sudo npm run build
 git add .
-git commit -m "deploy"
-git push heroku main
+git commit -m 'adding dist subtree'
+git subtree push --prefix dist origin gh-pages
+git commit -m 'adding dist subtree'
+git push
